e99b418a580fa77b0a818f7d80f0f2e36d70a14f
hugorey32 <102004246+hugorey32@users.noreply.github.com>
2022-03-26 23:21:10 -0600

    Transform, else, if

--- juego_memoria.js
+++ juego_memoria.js
@@ -1,10 +1,29 @@
-
-
 var cuerpo = document.getElementById('cuerpo');
 
+
 cuerpo.addEventListener('click',function(e){
-    document.getElementById('caraFrente').style.transition = "opacity 2s";
-    document.getElementById('caraFrente').style.opacity = "0%";
-    document.getElementById('caraAtras').style.transition = "opacity 2s";
+    var caraFrente = document.getElementById('caraFrente');
+
+    if(!caraFrente.innerHTML)
+    {
+        caraFrente.style.transition = "opacity 1s";
+        caraFrente.style.opacity = "100%";
+        caraFrente.innerHTML = `<img src="https://scontent.fmex16-1.fna.fbcdn.net/v/t1.6435-9/109119284_1377724675760437_6163195043215968101_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=e3f864&_nc_ohc=jvrfgloWoVAAX8ypw9m&_nc_ht=scontent.fmex16-1.fna&oh=00_AT_V8QCwXVeZF9-Rir-X07RVckVpqU5E8MVgxGI2-pN4mQ&oe=625E6569" 
+        alt="LSM ASL" style="width: 150px; height: 200px;">`;
+        document.getElementById('caraAtras').innerHTML = ``;
+        document.getElementById('caraAtras').style.transition = "opacity 1s";
+        document.getElementById('caraAtras').style.opacity = "0%";
+    } else {
+        caraFrente.style.transition = "opacity 1s";
+        caraFrente.style.opacity = "0%";
+        caraFrente.innerHTML = ``;
+        document.getElementById('caraAtras').innerHTML = `<img src="https://i.pinimg.com/236x/15/35/4c/15354cf0b58abb4b7f1047efeb240159.jpg"
+        alt="LSM A" style="width: 150px; height: 200px;">`;
+        document.getElementById('caraAtras').style.position="absolute";
+    document.getElementById('caraAtras').style.transition="transform 1s";
+    document.getElementById('caraAtras').style.transform="rotateY(180deg)";
     document.getElementById('caraAtras').style.opacity = "100%";
+    }
+
+
 })
